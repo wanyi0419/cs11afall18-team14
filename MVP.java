1480d9c843a74503b9a7bde90bc172faa8ea4f10
chena2022 <chena@brandeis.edu>
2018-11-06 18:53:17 -0500

    MVP

---
+++ MVP.java
@@ -0,0 +1,7 @@
+public class MVP {
+  public static void main(String[]args) {
+    System.out.println("What is your name?");
+    String name = TextIO.getln();
+    System.out.println("Hello " + name);
+  }
+}
