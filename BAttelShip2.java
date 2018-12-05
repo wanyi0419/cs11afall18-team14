92aa94f6b066e1bbb7e5d68ad2a3fffd5bbd560f
chena2022 <chena@brandeis.edu>
2018-12-05 00:46:34 -0500

    Wrap around and check for position
    I added a wrap around and checked for position.

--- BAttelShip2.java
+++ BAttelShip2.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class BAttelShip2 
+public class BAttelShip2
 {
 	Scanner console = new Scanner(System.in);
 	static int x1=0;
@@ -20,8 +20,8 @@ public class BAttelShip2
 	static int maxHit=0;
 	static int maxHitc=0;
 
-	
-	public static void main(String[] args) throws Exception 
+
+	public static void main(String[] args) throws Exception
 	{
 		// TODO Auto-generated method stub
 		System.out.print("In this game you will first set up your Battleship. Then if you hit the enemy's ship, you win, if the enemy hit your ship, you lose.");
@@ -30,18 +30,18 @@ public class BAttelShip2
 		System.out.println("");
 		StartGame();
 	}
-	
-	
+
+
 	public static void StartGame() throws Exception
-	{   
+	{
 		System.out.print("Now set up your Battleship.");
 	    System.out.println();
-		Scanner console = new Scanner(System.in);	
+		Scanner console = new Scanner(System.in);
 	    System.out.print("Enter the position x:");
 	    int x1 = console.nextInt()-1;
 	    System.out.print("Enter the position y:");
 	    int y1 = console.nextInt()-1;
-	    user.setPosx(x1); 
+	    user.setPosx(x1);
 	    user.setPosy(y1);
 	    x1 = user.getPosx();
 		y1 = user.getPosy();
@@ -52,20 +52,20 @@ public class BAttelShip2
 		comp.setPosy(y2);
 		x2 = comp.getPosx();
 		y2 = comp.getPosy();
-		
-		for(int i=0;i<map.length;i++) 
+
+		for(int i=0;i<map.length;i++)
 		{
-			for(int j=0; j<map[0].length; j++) 
+			for(int j=0; j<map[0].length; j++)
 			{
 				System.out.print(map[i][j]);
 			}
 			System.out.println();
 		}
-		
+
 		Hit();
 	}
-	
-	
+
+
 	public static void Hit () throws Exception
 	{
 		System.out.print("Round"+" "+count);
@@ -74,14 +74,18 @@ public class BAttelShip2
 		System.out.print("Your Turn Now!!!! Please choose where to hit. (enter x position and y position)");
 		Hit1x = console.nextInt()-1	;
 	    Hit1y = console.nextInt()-1;
+			System.out.println("Hit1x: " + Hit1x + " Hit1y: " + Hit1y);
+			if((Hit1x > 4 || Hit1x < 0) || (Hit1y > 4 || Hit1y < 0)) {
+        System.out.println("That entry is out of bounds. You lose a turn");
+      }
 	    Hit2x = (int) (Math.random()*5);
 	    Hit2y = (int) (Math.random()*5);
-	   
+
 	    x1 = user.getPosx();
 		y1 = user.getPosy();
-	    x2 = comp.getPosx(); 
+	    x2 = comp.getPosx();
 		y2 = comp.getPosy();
-		
+
 	    if (Hit1x == x2 && Hit1y == y2)
 		{
 			System.out.println("You win");
@@ -93,27 +97,27 @@ public class BAttelShip2
 			System.out.println("You lose");
 			end();
 		}
-		else 
+		else
 		{
 			count++;
 			System.out.print("The enemy has attacked");
 			System.out.println();
-			if (count>=3 && count%3==0) 
+			if (count>=3 && count%3==0)
 			{
 				MaxHit();
 			}
-			if (count>=15) 
+			if (count>=15)
 			{
 				tie();
 			}
-			else 
+			else
 			{
 				move();
 			}
 		}
 	}
-	
-	public static void MaxHit() throws Exception 
+
+	public static void MaxHit() throws Exception
 	{
 		x2=comp.getPosx();
 		y2=comp.getPosy();
@@ -123,43 +127,94 @@ public class BAttelShip2
 		System.out.print("Here comes the Maximum hit. Now please choose a cross line to eliminate!!!! (enter the cross line you want to hit)");
 		maxHit =  console.nextInt()-1;
 		maxHitc =(int) (Math.random()*5);
-		if (maxHit == x2 || maxHit == y2) 
+		if (maxHit == x2 || maxHit == y2)
 		{
 			System.out.println("You win");
 			end();
 		}
-		if (maxHit == x1 || maxHitc == y1) 
+		if (maxHit == x1 || maxHitc == y1)
 		{
 			System.out.println("You lose");
 			end();
 		}
-		else 
+		else
 		{
 			move();
 		}
 	}
-	
-	
-	public static void move() throws Exception 
+
+
+	public static void move() throws Exception
 	{
 		Scanner console = new Scanner(System.in);
 		System.out.println("Your turn to move, Please enter a new position (enter up, down, left, right).");
 		String move1 = console.next();
 		System.out.println("enemy has moved his ship move.");
-		switch (move1) 
+		switch (move1)
 		{
-		case "up": user.setPosx(x1-1); break;
-		case "down": user.setPosx(x1+1); break;
-		case "left": user.setPosy(y1-1); break;
-		case "right": user.setPosy(y1+1); break;
+		case "up":
+			x1--;
+			if(x1 < 0) {
+				x1 = 4;
+			}
+			user.setPosx(x1);
+			break;
+		case "down":
+			x1++;
+			if(x1>4) {
+				x1 = 0;
+			}
+			user.setPosx(x1);
+			break;
+
+		case "left":
+			y1--;
+			if(y1 < 0) {
+				y1 = 4;
+			}
+			user.setPosy(y1);
+			break;
+
+		case "right":
+			y1++;
+			if(y1 > 4) {
+				y1=0;
+			}
+			user.setPosy(y1);
+			break;
 		}
 		int move2 = (int) (Math.random()*5);
-		switch (move2) 
+		switch (move2)
 		{
-		case 0: comp.setPosy(y2-1); break;
-		case 1: comp.setPosy(y2+1); break; 
-		case 2: comp.setPosx(x2-1); break;
-		case 3: comp.setPosx(x2+1); break;
+		case 0:
+			y2--;
+			if(y2 < 0) {
+				y2 = 4;
+			}
+			comp.setPosy(y2);
+			break;
+
+		case 1:
+			y2++;
+			if(y2 > 4) {
+				y2 = 0;
+			}
+			comp.setPosy(y2);
+			break;
+		case 2:
+			x2--;
+			if(x2 < 0) {
+				x2 = 4;
+			}
+			comp.setPosx(x2);
+			break;
+		case 3:
+			x2++;
+			if(x2 > 4) {
+				x2 = 0;
+			}
+			comp.setPosx(x2);
+			break;
 		}
 		x1=user.getPosx();
 		y1=user.getPosy();
@@ -177,27 +232,27 @@ public class BAttelShip2
 		}
 	    Hit();
 	}
-	
-	
-	public static void tie() throws Exception 
+
+
+	public static void tie() throws Exception
 	{
 		System.out.print("TIE!!!!!!!!");
 		end();
 	}
-	
-	
+
+
 	public static void end() throws Exception
 	{
 		System.out.println("The Game has ended. Do you want to play again?");
 		Scanner console = new Scanner(System.in);
 		System.out.println("please enter yes or no");
 		String judge = console.next();
-		if (judge.equals("yes")) 
+		if (judge.equals("yes"))
 		{
 			count=0;
 			StartGame();
 		}
-		else  
+		else
 		{
 			System.out.println("GoodBye");
 			throw new Exception("test exception");
